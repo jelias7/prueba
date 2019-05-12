5fcc6c2b3e581c459666a4c9339e062e7681050a
Juan Rosario <juanelias18@hotmail.com>
2019-05-12 12:21:21 -0400

    subiendolos

---
+++ prueba/cap1/Ejercicio1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpEjercicios1
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Juan Elias");
+            Console.Read();
+        }
+    }
+}
---
+++ prueba/cap1/Ejercicio5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpEjercicios1
+{
+    class Ejercicio5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Juan Elias");
+            Console.WriteLine("Rosario Mena");
+            Console.WriteLine("2016-0631");
+            Console.Read();
+        }
+    }
+}
